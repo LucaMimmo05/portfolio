bbf0480a64a36595defe2cf21dba1cbd93a79ef7
LucaMimmo05 <171295375+LucaMimmo05@users.noreply.github.com>
2025-07-09 17:44:49 +0200

    Created about me section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import style from "./App.module.css";
 import Overlay from "./components/overlay/Overlay";
 import Navbar from "./components/navbar/Navbar";
+import Carousel from "./components/carousel/Carousel";
 
 const App = () => {
     const handleClick = (section) => {
@@ -25,13 +26,49 @@ const App = () => {
                 </main>
 
                 <div className={style.AppDiveIn}>
-                    <img src="../public/double-arrow-bottom-icon.svg" alt="" srcset="" />
+                    <img
+                        src="../public/double-arrow-bottom-icon.svg"
+                        alt=""
+                        srcset=""
+                    />
                     <h4>DIVE IN</h4>
-                    <img src="../public/double-arrow-bottom-icon.svg" alt="" srcset="" />
+                    <img
+                        src="../public/double-arrow-bottom-icon.svg"
+                        alt=""
+                        srcset=""
+                    />
                 </div>
             </section>
 
-            <section id="about" className={style.AppAbout}></section>
+            <section id="about" className={style.AppAbout}>
+                <Overlay top={-150} left={900} right={0} bottom={0} />
+                <div className={style.AppAboutLeft}>
+                    <div className={style.AppAboutLeftText}>
+                        <h3>I'm</h3>
+                        <div className={style.AppAboutLeftTitle}>
+                            <h1>Luca</h1>
+                            <h1>Mimmo</h1>
+                        </div>
+                        <h3>Junior Web Developer</h3>
+                    </div>
+                    <hr />
+                    <p>
+                        Hi, I'm Luca Mimmo — I love building websites, designing
+                        clean interfaces, and learning new tech every day.
+                        Always working on cool side projects.
+                    </p>
+                </div>
+                <div className={style.AppAboutRight}>
+                    <div className={style.AppAboutRightContent}>
+                        <div className={style.AppAboutRightImage}></div>
+                        <div className={style.AppAboutRightSkills}>
+                            <h3>Skills</h3>
+                            <Carousel/>
+                        </div>
+                        
+                    </div>
+                </div>
+            </section>
             <section id="projects" className={style.AppProjects}></section>
             <section id="contact" className={style.AppContact}></section>
         </div>
