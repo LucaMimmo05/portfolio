f89d1b82daf7e1d6f7190e3da051f49bc74de74e
LucaMimmo05 <171295375+LucaMimmo05@users.noreply.github.com>
2025-07-21 19:55:24 +0200

    Added mobile view for footer

--- src/components/sections/Footer/Footer.jsx
+++ src/components/sections/Footer/Footer.jsx
@@ -26,7 +26,7 @@ const Footer = () => {
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
                         width="14"
-                        height="15"
+                        height="14"
                         viewBox="0 0 14 15"
                         fill="none"
                     >
